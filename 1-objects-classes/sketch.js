4f458cacafc00c61e721a5390752a9ed81585569
sophiagnuse <sophiagnuse@gmail.com>
2019-10-17 13:53:59 -0700

    new stuff

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -2,15 +2,18 @@
 let b;
 let anotherBall;
 
+let SW;
+
 function setup() {
   createCanvas(800, 400);
-  b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  b = new Ball(0, 100,"red",20); //make a new ball from the Ball class and call it b.
+  anotherBall = new Ball(200,20,"green",30);
+  SW = new Ball(0, 100, "pink", 50)
 }
 
 
 function draw(){
-	background(220);
+	background("green");
     b.drawBall(); //draw the ball called b (go look in the Ball class for the drawBall function)
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
@@ -22,15 +25,17 @@ function draw(){
 //ball class from which to create new balls with similar properties.
 class Ball {
 
-	constructor(x,y,color){ //every ball needs an x value and a y value
+	constructor(x,y,color,diameter){ //every ball needs an x value and a y value
 		    this.x = x;
     		this.y = y;
         this.color= color;
+        this.diameter=diameter;
+
 	}
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    ellipse(this.x,this.y,this.diameter,this.diameter);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,6 +4,24 @@ function setup() {
 }
 
 function draw(){
-	background (100,100,3);
+	background (100,200,30);
+  class Ball {
+    constructor(x,y,color){
+    		this.x = x;
+        		this.y = y;
+               this.color = color;
+    	  }
+        drawBall(){
+            	   stroke(0);
+            	   fill(this.color);
+                 ellipse(this.x,this.y,10,10);
+        	}
+
+        moveBall(){
+        	   this.x = this.x+2;
+        	   this.y = this.y+.5;
+
+ // here is where you’ll put the instructions for making balls
+}
 
 }
