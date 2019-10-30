f3beaaf604fe918e43e3cf6e077fd0cb3eeca77c
sophiagnuse <sophiagnuse@gmail.com>
2019-10-29 18:21:21 -0700

    Update sketch.js

--- 3-conditional-statement/sketch.js
+++ 3-conditional-statement/sketch.js
@@ -1,12 +1,17 @@
 //create an empty array called balls
-let balls = [];
+let squares = [];
 
 //create a variable to hold your avatar
 let me;
 
+let coloroptions = []
+
+
 
 function setup() {
   createCanvas(500, 400);
+  coloroptions = [color(140, 0, 255), color(255, 238, 0), color(255, 102, 0), color(255, 0, 166), color(26, 255, 0)]
+
 
   //make one avatar called me
   me = new Avatar(width/2, 300, 3);
@@ -14,22 +19,22 @@ function setup() {
 }
 
 function draw(){
-	background(220);
+	background(0, 0, 0);
 
   me.drawMe();
   me.moveMe();
 
-  if (frameCount % 25 == 0) {
-      let  b = new Ball(width, random(0,height), -3);
-      balls.push(b);
-      console.log(balls); //print the balls array to the console
+  if (frameCount % 1/999 == 0) {
+      let  b = new Square(width, random(0,height), -3);
+      squares.push(b);
+      console.log(squares); //print the balls array to the console
     }
 
 //	draw all the balls in that array
-	for (let i = 0; i < balls.length; i++) {
-	 	      balls[i].drawBall();
-       	  balls[i].moveBall();
-        	balls[i].bounceBall();
+	for (let i = 0; i < squares.length; i++) {
+	 	      squares[i].drawSquare();
+       	  squares[i].moveSquare();
+          squares[i].bounceSquare();
 	  }
 
 }
@@ -44,9 +49,9 @@ class Avatar {
 	}
 
 	drawMe(){  // draw the running person
-    		stroke("green");
+    		stroke(170,170,170);
         strokeWeight(3);
-    		fill("blue");
+    		fill("white");
 		    ellipse(this.x,this.y,20,20);
         line(this.x,this.y, this.x, this.y+40);
         line(this.x, this.y+40, this.x-20, this.y+60);
@@ -63,18 +68,20 @@ class Avatar {
 
     if (keyIsDown(DOWN_ARROW)) { // if you hold the down arrow, move down by speed
         this.y += this.speed;
+
+
+        }
+      }
     }
-	}
 
-  die(){
 
-  }
 
-}
+
+
 
 
 //ball class from which to create new balls with similar properties.
-class Ball {
+class Square {
 
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
@@ -84,21 +91,21 @@ class Ball {
 	}
 
 	// draw a ball on the screen at x,y
-	drawBall(){
+	drawSquare(){
     	stroke(0);
       strokeWeight(1);
-    	fill("red");
-		  ellipse(this.x,this.y,10,10);
+    	fill(coloroptions[int(random(0,coloroptions.length))]);
+		  rect(this.x,this.y,20,20);
 	}
 
 	//update the location of the ball, so it moves across the screen
-	moveBall(){
+	moveSquare(){
 		this.x = this.x+ this.speed;
 		this.y = this.y+.5;
 	}
 
 	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
-  	bounceBall(){
+  	bounceSquare(){
     		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
       			this.speed = -this.speed;
     		}
